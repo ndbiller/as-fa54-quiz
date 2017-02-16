d96ba128d4edd4ea350cebfab42ac8b8ad9d4244
Sevpfl <sev@erratic-ink.com>
2017-02-16 14:56:04 +0100

    Donnerstag

---
+++ quiz/quiz/Questrion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace quiz
+{
+    class Questrion
+    {
+        int Id { get; set; }
+        string QuestionText { get; set; }
+        IList<string> Answers { get; set; }
+        int CorrectAnswer { get; set; }
+        int Selected { get; set; }
+
+        Questrion(int id, string questionText, IList<string> answers, int correctAnswer)
+        {
+            Id = id;
+            QuestionText = questionText;
+            Answers = answers;
+            CorrectAnswer = correctAnswer;
+            Selected = 999;
+        }
+
+        public bool Solve()
+        {
+
+            if (Selected == CorrectAnswer)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
---
+++ quiz/quiz/Questrionaire.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace quiz
+{
+    class Questrionaire
+    {
+        int Id { get; set; }
+        IList<Questrion> Questions { get; set; }
+
+        Questrionaire(IList<Questrion> questions)
+        {
+            Questions = questions;
+        }
+
+        public bool Evaluate(decimal passThreshhold)
+        {
+            foreach (Questrion q in Questions)
+            {
+                q.Solve();
+            }
+            return true;
+        }
+
+    }
+}
