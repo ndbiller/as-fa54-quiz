a5a78c8b648349534d60e29e11528f663f9a82f4
ndbiller <andie.biller@gmail.com>
2017-04-07 19:05:21 +0200

    results page added

---
+++ quiz/quiz/ResultsPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Data;
+using System.Xml;
+using System.Configuration;
+using quiz.Models;
+
+namespace quiz
+{
+    /// <summary>
+    /// Interaction logic for ResultsPage.xaml
+    /// </summary>
+    public partial class ResultsPage : Page
+	{
+        public ResultsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
