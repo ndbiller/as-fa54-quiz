ea7a2d49105f7055e4e2a719a7759114aa825e34
B3ttm3nsch <krueger.rico@web.de>
2017-04-25 23:20:49 +0200

    small changes

--- quiz/quiz/Models/DataReader.cs
+++ quiz/quiz/Models/DataReader.cs
@@ -13,29 +13,47 @@ namespace quiz.Models
 
         static public int[] GetQuestionIds(int id)
         {
-            var result = binnenschifffahrt.T_Fragebogen_unter_Maschine.Where(a => a.FragebogenNr == id).ToList();
-            Trace.WriteLine("Results: " + result.Count);
-            //result r.id -> int array
-            return new int[] {2, 3};
-        }
+            //    int[] ids;
+            //    var query = binnenschifffahrt.T_Fragebogen_unter_Maschine.Where(a => a.FragebogenNr == id).ToList();
+            //    ids = new int[query.Count - 1];
+            //    for (int i = 0; i < ids.Length; i++)
+            //    {
 
-        public static Question GetQuestion(string dbname, int id)
-        {
-            switch (dbname)
+            //    }
+
+            //    Trace.WriteLine("Results: " + result.Count);
+
+            //    //result r.id -> int array
+            //    return new int[] {2, 3};
+
+            int[] query = binnenschifffahrt.T_Fragebogen_unter_Maschine.Where(no => no.FragebogenNr == id).Select(p => p.F_Id_SBF_Binnen).ToArray();
+            for (int i = 0; i < query.Length; i++)
             {
-                case "binnen":
-                    quiz.T_SBF_Binnen question = binnenschifffahrt.T_SBF_Binnen.Where(a => a.P_Id == r.F_Id_SBF_Binnen).SingleOrDefault();
-                    return new Question(question.P_Id,,,,)
-
-                    break;
-                case "ubi":
-                    int a = 0;
-                    break;
-                default:
-                    int u = 0;
-                    break;
+                Trace.WriteLine("dr" + query[i]);
             }
-
+            return query;
+            //return query;
+            
+            //return new int[] { 2, 3 };
         }
+
+        //public static Question GetQuestion(string dbname, int id)
+        //{
+        //    switch (dbname)
+        //    {
+        //        case "binnen":
+        //            quiz.T_SBF_Binnen question = binnenschifffahrt.T_SBF_Binnen.Where(a => a.P_Id == r.F_Id_SBF_Binnen).SingleOrDefault();
+        //            return new Question(question.P_Id,,,,)
+
+        //            break;
+        //        case "ubi":
+        //            int a = 0;
+        //            break;
+        //        default:
+        //            int u = 0;
+        //            break;
+        //    }
+
+        //}
     }
 }
